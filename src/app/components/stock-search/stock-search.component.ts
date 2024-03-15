57dc434c3999cd5cbb96f09f645f13b05f9683ff
Mansi Garg <mansigarg@Mansis-MacBook-Pro.local>
2024-03-15 12:38:49 -0700

    basic e-2-e integration

--- src/app/components/stock-search/stock-search.component.ts
+++ src/app/components/stock-search/stock-search.component.ts
@@ -1,26 +1,28 @@
 import { Component, Inject } from '@angular/core';
 import { StockSearchService } from '../../core/services/stock-search.service';
-import { HttpClientModule } from '@angular/common/http';
 import { StockDetailsComponent } from '../stock-details/stock-details.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stock-search',
   standalone: true,
-  imports: [CommonModule, StockDetailsComponent],
+  imports: [StockDetailsComponent, CommonModule],
   templateUrl: './stock-search.component.html',
   styleUrls: ['./stock-search.component.css'],
 })
 export class StockSearchComponent {
-  stockInfo: any = null; // Property to hold fetched stock data
+  stockInfo: any = null;
 
   constructor(private stockSearchService: StockSearchService) {}
 
   searchStock(stock: string) {
     this.stockSearchService.searchStock(stock).subscribe({
-      next: (response: any) => {
-        this.stockInfo = response; // Store fetched data
-        console.log(response);
+      next: (results) => {
+        this.stockInfo = {
+          company: results.companyInfo,
+          price: results.stockPriceDetails,
+        };
+        console.log(this.stockInfo);
       },
       error: (error: any) => {
         console.error('Error fetching stock data:', error);
--- src/app/core/services/stock-search.service.ts
+++ src/app/core/services/stock-search.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { forkJoin, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StockSearchService {
   constructor(private http: HttpClient) {}
+  private baseUrl = 'http://localhost:8000/api';
 
-  searchStock(stock: string) {
-    return this.http.get(
-      `http://localhost:8000/api/company?symbol=${encodeURIComponent(stock)}`
+  searchStock(stock: string): Observable<any> {
+    const companyInfo = this.http.get(
+      `${this.baseUrl}/company?symbol=${encodeURIComponent(stock)}`
     );
+    const stockPriceDetails = this.http.get(
+      `${this.baseUrl}/latestPrice?symbol=${encodeURIComponent(stock)}`
+    );
+    return forkJoin({ companyInfo, stockPriceDetails });
   }
 
   clearStock() {
