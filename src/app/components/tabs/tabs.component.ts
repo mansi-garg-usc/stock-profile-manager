4e362397bccb7122c901cb52f7e6953d47f7d1c2
Mansi Garg <mansigarg@Mansis-MacBook-Pro.local>
2024-03-19 18:10:42 -0700

    tabs component

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SummaryComponent } from '../summary/summary.component';
+import { TopNewsComponent } from '../top-news/top-news.component';
+import { InsightsComponent } from '../insights/insights.component';
+import { ChartsComponent } from '../charts/charts.component';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [NgbNavModule], // Import the NgbNavModule here
+  imports: [
+    NgbNavModule,
+    MatTabsModule,
+    SummaryComponent,
+    TopNewsComponent,
+    InsightsComponent,
+    ChartsComponent,
+  ], // Import the NgbNavModule here
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.css'],
 })
